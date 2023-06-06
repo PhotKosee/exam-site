b265e8ff8707d6aeae5eaa7f2a6b486b5ee435b5
PhotKosee <kosee.phot@gmail.com>
2023-06-06 05:56:19 -0400

    Create submission view and evaluate + template showing results

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-# <HINT> Import any new Models here
-from .models import Course, Enrollment
+from .models import Course, Enrollment, Question, Choice, Submission
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
@@ -103,34 +102,54 @@ def enroll(request, course_id):
     return HttpResponseRedirect(reverse(viewname='onlinecourse:course_details', args=(course.id,)))
 
 
-# <HINT> Create a submit view to create an exam submission record for a course enrollment,
-# you may implement it based on following logic:
-         # Get user and course object, then get the associated enrollment object created when the user enrolled the course
-         # Create a submission object referring to the enrollment
-         # Collect the selected choices from exam form
-         # Add each selected choice object to the submission object
-         # Redirect to show_exam_result with the submission id
-#def submit(request, course_id):
+def submit(request, course_id):
+    user = request.user
+    course = get_object_or_404(Course, pk=course_id)
+    enrollement = Enrollment.objects.get(user = user, course = course)
+    answears = extract_answers(request)
+    submission = Submission.objects.create(enrollment= enrollement)
+    for answear in answears:
+        submission.choices.add(answear)
+
+    return HttpResponseRedirect(reverse(viewname='onlinecourse:show_exam_result', args=(course.id,submission.id)))
 
 
-# <HINT> A example method to collect the selected choices from the exam form from the request object
-#def extract_answers(request):
-#    submitted_anwsers = []
-#    for key in request.POST:
-#        if key.startswith('choice'):
-#            value = request.POST[key]
-#            choice_id = int(value)
-#            submitted_anwsers.append(choice_id)
-#    return submitted_anwsers
+def extract_answers(request):
+    submitted_anwsers = []
+    for key in request.POST:
+        if key.startswith('choice'):
+            value = request.POST[key]
+            choice_id = int(value)
+            submitted_anwsers.append(choice_id)
+    return submitted_anwsers
 
 
-# <HINT> Create an exam result view to check if learner passed exam and show their question results and result for each question,
-# you may implement it based on the following logic:
-        # Get course and submission based on their ids
-        # Get the selected choice ids from the submission record
-        # For each selected choice, check if it is a correct answer or not
-        # Calculate the total score
-#def show_exam_result(request, course_id, submission_id):
+def show_exam_result(request, course_id, submission_id):
+    course = get_object_or_404(Course, pk=course_id)
+    submission = get_object_or_404(Submission, pk=submission_id)
+    context = {}
+    user_score = 0
+    total_score = 0
+    user_choices = []
 
+    for question in Question.objects.filter(courses = course.id ):
+        total_score += question.grade
 
+    for item in submission.choices.all():
+        user_choices.append(item)
+        if item.correct:
+            all_answers = Choice.objects.filter(questions = item.questions.id, correct = True)
+            if len(all_answers) > 0:
+                user_score += get_object_or_404(Question, pk=item.questions.id).grade / len(all_answers)
+            else:
+                user_score += get_object_or_404(Question, pk=item.questions.id).grade
+        
+    try:
+        user_score = round(user_score / total_score * 100)
+    except:
+        user_score = 0
 
+    context['user_choices'] = user_choices
+    context['course'] = course
+    context['user_score'] = user_score
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
